e62953f2b37115980fc8cca75d85e79ec914aacc
illiptic <jeremy.davis42@gmail.com>
2017-08-25 15:38:59 +0200

    Clean unused assets and fix build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -18,7 +18,7 @@ module.exports = {
         'NODE_ENV': JSON.stringify('production')
       }
     }),
-    new ExtractTextPlugin("[name].css")
+    new ExtractTextPlugin("styles.css")
   ],
   module: {
     loaders: [
